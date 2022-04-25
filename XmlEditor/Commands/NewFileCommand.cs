03cb714aeb65ca60ac2646549eb218232b5b0670
Francis Kus <francistkus@gmail.com>
2022-04-25 19:34:11 -0400

    add newfilecommand

---
+++ XmlEditor/Commands/NewFileCommand.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace XmlEditor.Commands
+{
+    internal class NewFileCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (App.Instance.IsOpen)
+            {
+                if (!App.PromptUnsavedChanges()) { return; }
+            }
+
+            var sfd = new SaveFileDialog();
+
+            if (sfd.ShowDialog() == true)
+            {
+                App.Instance.NewFile(sfd.FileName);
+            }
+        }
+    }
+}
